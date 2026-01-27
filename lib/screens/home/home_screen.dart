9bc80ea10d5917b69e88ee12513c93a29752424c
tee2490 <44152787+tee2490@users.noreply.github.com>
2026-01-27 13:44:32 +0700

    Section02 Style Text() widgets

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,22 +10,30 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: ListView(
         children: [
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Column(
-                    children: [Text("Good morning"), Text("Book Tickets")],
-                  ),
-                  Container(color: Colors.red, width: 100, height: 70),
-                ],
-              ),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [Text("Search icon"), Text("Empty space")],
-              ),
-            ],
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Good morning", style: AppStyles.headLineStyle3),
+                        const SizedBox(height: 5),
+                        Text("Book Tickets", style: AppStyles.headLineStyle1),
+                      ],
+                    ),
+                    Container(color: Colors.red, width: 100, height: 70),
+                  ],
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text("Search icon"), Text("Empty space")],
+                ),
+              ],
+            ),
           ),
         ],
       ),
