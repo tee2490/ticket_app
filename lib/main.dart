98fdbe49a3e8a3fe5ebe51812b56255b1fb15179
tee2490 <44152787+tee2490@users.noreply.github.com>
2026-01-28 08:55:41 +0700

    Section02 Understand copyWith method and create examples

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,22 @@ import 'package:ticket_app/base/bottom_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
+  var test = TestClass(x: 4, y: 10);
+  print('x = ${test.x}');
+
+  var newTest = test.copyWith(x: 30);
+  print('x=${newTest.x} y = ${newTest.y}');
+}
+
+class TestClass {
+  int x;
+  int y;
+
+  TestClass({required this.x, required this.y});
+
+  TestClass copyWith({int? x, int? y}) {
+    return TestClass(x: x ?? this.x, y: y ?? this.y);
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -10,10 +26,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(),
       home: BottomNavBar(),
     );
   }
