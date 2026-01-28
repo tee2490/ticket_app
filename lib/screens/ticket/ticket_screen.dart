3feecdcb53a099f628f5e73c109531faec82d272
tee2490 <44152787+tee2490@users.noreply.github.com>
2026-01-28 17:31:54 +0700

    Section05 Show the rest of the ticket detail

--- lib/screens/ticket/ticket_screen.dart
+++ lib/screens/ticket/ticket_screen.dart
@@ -1,8 +1,11 @@
+import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/media.dart';
 import 'package:ticket_app/base/res/app_styles.dart';
 import 'package:ticket_app/base/utils/all_json.dart';
 import 'package:ticket_app/base/widgets/app_column_text_layout.dart';
+import 'package:ticket_app/base/widgets/app_layoutbuilder_widget.dart';
 import 'package:ticket_app/base/widgets/ticket_view.dart';
 import 'package:ticket_app/screens/search/widgets/app_ticket_tabs.dart';
 
@@ -21,18 +24,19 @@ class TicketScreen extends StatelessWidget {
           const SizedBox(height: 20),
           const AppTicketTabs(firstTab: "Upcoming", secondTab: "Previous"),
           const SizedBox(height: 20),
+          //white and black ticket
           Container(
             padding: const EdgeInsets.only(left: 16),
             child: TicketView(ticket: ticketList[0], isColor: true),
           ),
-          SizedBox(height: 1),
+          const SizedBox(height: 1),
           Container(
             margin: const EdgeInsets.symmetric(horizontal: 15),
             padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
             color: AppStyles.ticketColor,
-            child: const Column(
+            child: Column(
               children: [
-                Row(
+                const Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     AppColumnTextLayout(
@@ -49,8 +53,95 @@ class TicketScreen extends StatelessWidget {
                     ),
                   ],
                 ),
+                const SizedBox(height: 20),
+                const AppLayoutBuilderWidget(
+                  randomDivider: 15,
+                  width: 5,
+                  isColor: false,
+                ),
+                const SizedBox(height: 20),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AppColumnTextLayout(
+                      topText: "2465 658494046865",
+                      bottomText: "Number of E-ticket",
+                      alignment: CrossAxisAlignment.start,
+                      isColor: true,
+                    ),
+                    AppColumnTextLayout(
+                      topText: "B46859",
+                      bottomText: "Booking code",
+                      alignment: CrossAxisAlignment.end,
+                      isColor: true,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                const AppLayoutBuilderWidget(
+                  randomDivider: 15,
+                  width: 5,
+                  isColor: false,
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(AppMedia.visaCard, scale: 11),
+                            Text(" *** 2462", style: AppStyles.headLineStyle3),
+                          ],
+                        ),
+                        const SizedBox(height: 5),
+                        Text("Payment method", style: AppStyles.headLineStyle4),
+                      ],
+                    ),
+                    const AppColumnTextLayout(
+                      topText: "\$249.99",
+                      bottomText: "Price",
+                      alignment: CrossAxisAlignment.end,
+                      isColor: true,
+                    ),
+                  ],
+                ),
               ],
             ),
+          ),
+          SizedBox(height: 1),
+          //bottom of the ticket detail section
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 15),
+            padding: EdgeInsets.symmetric(vertical: 20),
+            decoration: BoxDecoration(
+              color: AppStyles.ticketColor,
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(21),
+                bottomLeft: Radius.circular(21),
+              ),
+            ),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: BarcodeWidget(
+                  height: 70,
+                  barcode: Barcode.code128(),
+                  data: 'https://www.dbestech.com',
+                  drawText: false,
+                  color: AppStyles.textColor,
+                  width: double.infinity,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          //colorful ticket
+          Container(
+            padding: const EdgeInsets.only(left: 16),
+            child: TicketView(ticket: ticketList[0]),
           ),
         ],
       ),
